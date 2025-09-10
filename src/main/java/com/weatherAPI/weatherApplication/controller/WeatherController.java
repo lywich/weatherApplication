d17cd9b2043420671b2c8a7b92a1230d3e68dbe1
lywich <lywichcy@gmail.com>
2025-09-10 15:45:54 +0800

    Add rate limiting to API

--- src/main/java/com/weatherAPI/weatherApplication/controller/WeatherController.java
+++ src/main/java/com/weatherAPI/weatherApplication/controller/WeatherController.java
@@ -1,5 +1,9 @@
 package com.weatherAPI.weatherApplication.controller;
 
+import java.time.Duration;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +12,9 @@ import com.weatherAPI.weatherApplication.model.WeatherRequest;
 import com.weatherAPI.weatherApplication.model.WeatherResponse;
 import com.weatherAPI.weatherApplication.service.WeatherService;
 
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
 import jakarta.validation.Valid;
 
 
@@ -15,14 +22,21 @@ import jakarta.validation.Valid;
 public class WeatherController {
 
     private final WeatherService weatherService;
+    private final Bucket bucket;
 
     public WeatherController(WeatherService weatherService) {
         this.weatherService = weatherService;
+        Bandwidth limit = Bandwidth.classic(5, Refill.greedy(1, Duration.ofMinutes(1)));
+        this.bucket = Bucket.builder().addLimit(limit).build();
     }
 
     @GetMapping("/weather")
-    public WeatherResponse getWeather(@Valid @ModelAttribute WeatherRequest req) {
-        return weatherService.getWeather(req);
+    public ResponseEntity<WeatherResponse> getWeather(@Valid @ModelAttribute WeatherRequest req) {
+        if (!bucket.tryConsume(1)) {
+            System.out.println("Token limit reached");
+            return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
+        }
+        return ResponseEntity.ok(weatherService.getWeather(req));
     }
 }
 
